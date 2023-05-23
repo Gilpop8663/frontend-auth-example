e3a639c857ea7cbc353483ada184900945afc33c
Gilpop8663 <wolfye@naver.com>
2023-05-23 12:05:48 +0900

    feat: 스탭1 적용

---
+++ my-app/src/App.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [data, setData] = useState();
+  const username = "user@example.com";
+  const password = "password1234";
+
+  const base64 = btoa(`${username}:${password}`);
+
+  useEffect(() => {
+    const access = async () => {
+      try {
+        const response = await fetch("http://localhost:3000", {
+          headers: {
+            Authorization: `Basic ${base64}`,
+          },
+        });
+
+        const result = await response.text();
+        setData(result);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    access();
+  }, [base64]);
+
+  return (
+    <div className="App">
+      <span>{data}</span>
+    </div>
+  );
+}
+
+export default App;
